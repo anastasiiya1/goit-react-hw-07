e1071e4ee5c87bfb0a0224dcd82a9d07d86b146d
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-06-15 20:10:04 +0300

    add axios

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external: ['axios'],
-    },
-  },
+  build: { sourcemap: true },
   resolve: {
     alias: {
       components: '/src/components',
